5da9797d0f519b9094ef689ee08321acabfcf568
ReturnTears <returntears@outlook.com>
2019-06-14 14:39:29 +0800

    微信公众号

---
+++ src/main/java/com/wx/gzh/AllstGzhApplication.java
@@ -0,0 +1,13 @@
+package com.wx.gzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllstGzhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllstGzhApplication.class, args);
+    }
+
+}
