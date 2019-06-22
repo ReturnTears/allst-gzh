616d326d4757bcafd612b70f2dac2b99d20bc48f
returntears <returntears@outlook.com>
2019-06-22 23:27:27 +0800

    修改thymeleaf模板

--- src/main/java/com/wx/gzh/web/WebController.java
+++ src/main/java/com/wx/gzh/web/WebController.java
@@ -2,6 +2,7 @@ package com.wx.gzh.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -17,5 +18,10 @@ public class WebController {
         ModelAndView view = new ModelAndView();
         view.setViewName("index");
         return view;
+    }
+
+    @RequestMapping(value = "inde", method = {RequestMethod.POST, RequestMethod.GET})
+    public String index() {
+        return "index";
     }
 }
